26d2ba0b5eec3c8c7de6042fe9b04ade014a1f55
Arthur Lino <arthur.ssLino@gmail.com>
2023-02-14 18:32:54 -0300

    🖌 styles: tailwind added | 📃feat: login basic html

---
+++ tailwind.config.cjs
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.astro"],
+  theme: {
+    colors: {
+      'l-red': '#F0414E',
+      'l-yellow': '#F3E7A9',
+      'l-white': '#FAFAFA',
+      'l-black': '#242424'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
